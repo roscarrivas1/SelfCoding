f317ffc7a2e0dd4cffe187bebb739b7f6de2cc2a
roscarrivas1 <64092699+roscarrivas1@users.noreply.github.com>
2020-04-25 14:19:53 -0700

    printf 1

---
+++ printf1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+	printf("Column A\tColumn B\tColumn C");
+	
+	printf("\nMy Computers\'s Beep Sounds Like This: \a!\n");
+	
+	printf("\"Letz\bs fix that typo and then show the backslash ");
+	
+	printf("character \\\" she said\n");
+	
+	return 0;
+}
