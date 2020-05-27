8ecf4178508f8aaf3987e4acb80e644834924462
roscarrivas1 <64092699+roscarrivas1@users.noreply.github.com>
2020-05-27 16:17:19 -0700

    Chapter 7 Example 1, with self made header
    Absolute Beginner's Guide to C, 3rd Edition

---
+++ Chapter7ex1.h
@@ -0,0 +1,15 @@
+// Example header program #1 from CHapter 7 of Absolute Beginner's
+// Guide to C, 3rd Edition
+// File Chapter7ex1.h
+
+// If you have certain values that will not change (or ony change
+// rarely)
+// you can set them with #DEFINE state-ments (so you can change them
+// as needed)
+
+// If you plan on using them in several programs, you can place them
+// in a header file
+
+#define KIDS 3
+#define FAMILY "They Peytons"
+#define MORGAGE_RATE 5.15
