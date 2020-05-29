a7087055858eece306085e6870a89bbe12e71296
roscarrivas1 <64092699+roscarrivas1@users.noreply.github.com>
2020-05-29 13:28:07 -0700

    Chapter 9 Example 2

---
+++ Chapter9ex2.c
@@ -0,0 +1,36 @@
+// Example program #2 fro Chapter 9 of Absolute Beginner's Guide to
+// C, 3rd Edition
+// File Chapter9ex2.c
+
+/* This program asks the user for a number of tires and price per
+tire. It then calculates a total price, adding sales tax. */
+
+// If you find you use a sales tax rate that may change, use #define
+// to set it in one place
+
+#include <stdio.h>
+#define SALESTAX .07
+
+int main(void)
+{
+    //Variables for the number of tires purchased, price,
+    // a before-tax total, and a total cost
+    // with tax
+    
+    int numTires;
+    float tirePrice, beforeTax, netSales;
+    
+    /* Get the numebr of tires purchased and price per tire. */
+    printf("How many tires did you purchase? ");
+    scanf(" %d", &numTires);
+    printf("What was the cost per tire (enter in $XX.XX format)? ");
+    scanf(" $%f", &tirePrice);
+    
+    /*Compute the price */
+    
+    beforeTax = tirePrice * numTires;
+    netSales = beforeTax + (beforeTax * SALESTAX);
+    printf("You spent $%.2f on your tires\n\n\n", netSales);
+    
+    return 0;
+}
